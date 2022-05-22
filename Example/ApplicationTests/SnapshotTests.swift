5e2cf72b513cf15a27f220d08817a15a4df9be09
Paul Zabelin <paulz@users.noreply.github.com>
2022-05-22 15:28:16 -0700

    better examples

--- Example/ApplicationTests/SnapshotTests.swift
+++ Example/ApplicationTests/SnapshotTests.swift
@@ -6,7 +6,7 @@ import SwiftUI
 class SnapshotTests: XCTestCase {
     func testContentView() throws {
         try verifySnapshot(FavoriteView_Previews.self)
-        try verifySnapshot(ContentView(), "content-view")
-        try verifySnapshot(Text("Hello World!").bold(), "example")
+        try verifySnapshot(ContentView())
+        try verifySnapshot(Text("SwiftUI").foregroundColor(.red), "example")
     }
 }
