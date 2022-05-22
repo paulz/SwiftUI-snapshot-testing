819fad837e50407deeb51aaa1c5082b3e79350d0
Paul Zabelin <paulz@users.noreply.github.com>
2022-05-22 05:28:35 -0700

    verify image sizes

--- Tests/SampleViewTest.swift
+++ Tests/SampleViewTest.swift
@@ -10,22 +10,16 @@ class SampleViewTest: XCTestCase {
         try XCTAssertSnapshot(SampleView(), "sampleSwiftUIView.png", colorAccuracy: 0.00002)
     }
     
-    func testSwiftUIRendersInWindow() throws {
+    func testImageSizeIsScaledFromExpected() throws {
         let image = try inWindowView(SampleView()) {
             $0.renderLayerAsBitmap()
         }
         XCTAssertEqual(image.size, expectedSize)
-        let pngData = try XCTUnwrap(image.pngData())
         let expectedData = try Data(
             contentsOf: folderUrl().appendingPathComponent("sampleSwiftUIView.png")
         )
         let expectedImage = try XCTUnwrap(UIImage(data: expectedData))
-        XCTAssertEqual(expectedImage.size, expectedSize.applying(.init(scaleX: 3, y: 3)))
-        
-        XCTContext.runActivity(named: "compare images") {
-            $0.add(.init(data: pngData, uniformTypeIdentifier: UTType.png.identifier))
-            let diff = compare(image, expectedImage)
-            XCTAssertEqual(0, diff.maxColorDifference(), accuracy: 0.00002)
-        }
+        let scale = UIScreen.main.scale
+        XCTAssertEqual(expectedImage.size, expectedSize.applying(.init(scaleX: scale, y: scale)))
     }
 }
