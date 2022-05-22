9367cf28d273af53f51591477309c9e07c16c9e7
Paul Zabelin <paulz@users.noreply.github.com>
2022-05-22 05:00:50 -0700

    rename UIKitViewTest

--- Tests/SampleViewTest.swift
+++ Tests/SampleViewTest.swift
@@ -3,16 +3,19 @@ import UniformTypeIdentifiers
 import SwiftUI_snapshot_testing
 
 class SampleViewTest: XCTestCase {
+    let expectedSize = CGSize(width: 30, height: 20)
+
     func testSwiftUIRendersInWindow() throws {
         let image = try inWindowView(SampleView()) {
             $0.renderLayerAsBitmap()
         }
-        XCTAssertEqual(image.size, .init(width: 30, height: 20))
+        XCTAssertEqual(image.size, expectedSize)
         let pngData = try XCTUnwrap(image.pngData())
         let expectedData = try Data(
             contentsOf: folderUrl().appendingPathComponent("sampleSwiftUIView.png")
         )
         let expectedImage = try XCTUnwrap(UIImage(data: expectedData))
+        XCTAssertEqual(expectedImage.size, expectedSize.applying(.init(scaleX: 3, y: 3)))
         
         XCTContext.runActivity(named: "compare images") {
             $0.add(.init(data: pngData, uniformTypeIdentifier: UTType.png.identifier))
---
+++ Tests/UIKitViewTest.swift
@@ -0,0 +1,32 @@
+import XCTest
+import SwiftUI
+import UniformTypeIdentifiers
+import SwiftUI_snapshot_testing
+
+class UIKitViewTest: XCTestCase {
+    let expectedSize = CGSize(width: 40, height: 20)
+    
+    lazy var sampleView: UIView = {
+        let frame = CGRect(origin: .zero, size: expectedSize)
+        let view = UIView(frame: frame)        
+        view.backgroundColor = .darkGray
+        let label = UILabel(frame: frame)
+        label.text = "Hello"
+        label.textColor = .yellow
+        view.addSubview(label)
+        return view
+    }()
+    
+    func testRenderLayerAsBitmapProducesSameData() throws {
+        let image = sampleView.renderLayerAsBitmap()
+        XCTAssertEqual(image.size, expectedSize)
+        let pngData = try XCTUnwrap(image.pngData())
+        let existing = try Data(
+            contentsOf: folderUrl().appendingPathComponent("sampleView.png")
+        )
+        XCTContext.runActivity(named: "compare image data") {
+            $0.add(.init(data: pngData, uniformTypeIdentifier: UTType.png.identifier))
+            XCTAssertEqual(existing, pngData)
+        }
+    }
+}
