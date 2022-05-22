d6de8c0da3e8982a8b4be7c25ec645bcfaca5e23
Paul Zabelin <paulz@users.noreply.github.com>
2022-05-22 15:01:53 -0700

    report color difference and overwrite expected
    fix: https://github.com/paulz/SwiftUI-snapshot-testing/issues/4

--- Sources/XCTAssertSnapshot.swift
+++ Sources/XCTAssertSnapshot.swift
@@ -8,27 +8,46 @@ public func XCTAssertSnapshot<V: View>(_ view: V, _ name: String? = nil, colorAc
         $0.renderLayerAsBitmap()
     }
     let isRunningOnCI = ProcessInfo.processInfo.environment.keys.contains("CI")
+    let shouldOverwriteExpected = !isRunningOnCI
     let pngData = try XCTUnwrap(image.pngData())
     let fileName = name ?? "\(V.self).png"
     let url = folderUrl(String(describing: file)).appendingPathComponent(fileName)
     if let expectedData = try? Data(contentsOf: url) {
         let expectedImage = try XCTUnwrap(UIImage(data: expectedData))
-        XCTContext.runActivity(named: "compare images") {
-            $0.add(.init(data: pngData, uniformTypeIdentifier: UTType.png.identifier))
+        try XCTContext.runActivity(named: "compare images") {
+            let actualImage = XCTAttachment(data: pngData, uniformTypeIdentifier: UTType.png.identifier)
+            actualImage.name = "actual image"
+            $0.add(actualImage)
             let diff = compare(image, expectedImage)
-            XCTAssertEqual(0, diff.maxColorDifference(), accuracy: colorAccuracy,
-                           "view image should match snapshot",
-                           file: file, line: line)
+            if diff.maxColorDifference() > colorAccuracy {
+                if shouldOverwriteExpected {
+                    try pngData.write(to: url)
+                }
+                XCTFail(
+                    """
+                    view did not match snapshot, overwriting expected
+                    some pixels were different by \(diff.maxColorDifference() * 100)% in color
+                    max allowed difference in color: \(colorAccuracy * 100)%
+                    see attached `difference` image between actual and expected
+                    """,
+                    file: file, line: line
+                )
+            }
+            let ciImage = diff.difference
+            let diffImage = CIContext().createCGImage(ciImage, from: ciImage.extent)
+            let diffAttachment = XCTAttachment(image: UIImage(cgImage: try XCTUnwrap(diffImage)))
+            diffAttachment.name = "difference"
+            $0.add(diffAttachment)
         }
     } else {
-        if isRunningOnCI {
-            XCTFail("missing snapshot: \(fileName), not recording on CI", file: file, line: line)
-        } else {
+        if shouldOverwriteExpected {
             try XCTContext.runActivity(named: "recording missing snapshot") {
                 $0.add(.init(data: pngData, uniformTypeIdentifier: UTType.png.identifier))
                 try pngData.write(to: url)
             }
             XCTFail("was missing snapshot: \(fileName), now recorded", file: file, line: line)
+        } else {
+            XCTFail("missing snapshot: \(fileName), not recording on CI", file: file, line: line)
         }
     }
 }
