dc051d5cf2aa936686e317c65e7fa33a3d199b38
Paul Zabelin <paulz@users.noreply.github.com>
2022-05-30 15:13:30 -0700

    update unit tests to use scale 1

--- Tests/SampleViewTest.swift
+++ Tests/SampleViewTest.swift
@@ -7,18 +7,23 @@ class SampleViewTest: XCTestCase {
     let expectedSize = CGSize(width: 30, height: 20)
 
     func testSwiftUIRendersInWindow() {
-        verifySnapshot(SampleView(), colorAccuracy: 0)
+        verifySnapshot(SampleView(), "SampleView-blank", colorAccuracy: 0)
     }
     
     func testImageSizeIsScaledFromExpected() throws {
         let image = try inWindowView(SampleView()) {
             $0.renderLayerAsBitmap()
         }
+        let url = folderUrl().appendingPathComponent("SampleView.png")
         XCTAssertEqual(image.size, expectedSize)
-        let expectedData = try Data(
-            contentsOf: folderUrl().appendingPathComponent("SampleView.png")
-        )
-        let expectedImage = try XCTUnwrap(UIImage(data: expectedData))
+        let expectedData = try Data(contentsOf: url)
+        let expectedImage = try XCTUnwrap(UIImage(data: expectedData, scale: 1))
+        let result = compare(image, expectedImage)
+        let colorDifference = result.maxColorDifference()
+        if colorDifference > 0 {
+            try image.pngData()?.write(to: url)
+        }
+        XCTAssertEqual(colorDifference, 0)
         XCTAssertEqual(expectedImage.size, expectedSize)
     }
 }
