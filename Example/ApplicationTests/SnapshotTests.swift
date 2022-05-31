8a60f919deb035f22974290cd11047d8e1c1f78d
Paul Zabelin <paulz@users.noreply.github.com>
2022-05-30 23:39:38 -0700

    trigger CI after changing base branch

--- Example/ApplicationTests/SnapshotTests.swift
+++ Example/ApplicationTests/SnapshotTests.swift
@@ -3,7 +3,6 @@ import XCTest
 import SwiftUI_snapshot_test
 import SwiftUI
 
-
 class SnapshotTests: XCTestCase {
     func testViews() throws {
         verifySnapshot(FavoriteView_Previews.self, colorAccuracy: 0)
