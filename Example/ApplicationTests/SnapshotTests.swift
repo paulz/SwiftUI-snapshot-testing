cbbe3bb88c22326c0b2edfb6a5470460f27afe17
Paul Zabelin <paulz@users.noreply.github.com>
2022-06-05 16:39:34 -0700

    fix: favorite view renders with 5% accuracy on GitHub runner

--- Example/ApplicationTests/SnapshotTests.swift
+++ Example/ApplicationTests/SnapshotTests.swift
@@ -6,7 +6,7 @@ import SwiftUI
 
 class SnapshotTests: XCTestCase {
     func testViews() throws {
-        verifySnapshot(FavoriteView_Previews.self, colorAccuracy: 0)
+        verifySnapshot(FavoriteView_Previews.self, colorAccuracy: 0.05)
         verifySnapshot(ContentView(), colorAccuracy: 0)
         verifySnapshot(Text("SwiftUI").foregroundColor(.red), "example", colorAccuracy: 0)
     }
