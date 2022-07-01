e04d0b39db8ca16d8de721682208d7bfc8553ccf
Paul Zabelin <paulz@users.noreply.github.com>
2022-07-01 15:24:57 -0700

    add VisualTestKit as binary target dependency

--- Package.swift
+++ Package.swift
@@ -14,13 +14,18 @@ let package = Package(
             name: "SwiftUI_SnapshotTesting",
             targets: ["ViewSnapshotTesting"]),
         .library(
+            name: "VisualTestKit",
+            targets: ["VisualTestKit"]),
+        .library(
             name: "SnapshotTestingPreviewGroup",
             targets: ["PreviewGroup"]),
     ],
     targets: [
         .target(
             name: "ViewSnapshotTesting",
-            dependencies: [.target(name: "PreviewGroup")],
+            dependencies: [
+                .target(name: "PreviewGroup")
+            ],
             path: "Sources/ViewSnapshotTesting",
             linkerSettings: [
                 .linkedFramework("XCTest", .when(platforms: [.iOS, .macOS]))
@@ -31,9 +36,16 @@ let package = Package(
             dependencies: [],
             path: "Sources/PreviewGroup"
         ),
+        .binaryTarget(
+            name: "VisualTestKit",
+            url: "https://github.com/paulz/VisualTestKit.framework/releases/download/v0.0.2/VisualTestKit.xcframework.zip",
+            checksum: "48b15eae813cde4136aa109fde05be1fe0dde6d28bc49b4e88f4fc294b926c2f"),
         .testTarget(
             name: "UnitTests",
-            dependencies: [.target(name: "ViewSnapshotTesting")],
+            dependencies: [
+                .target(name: "ViewSnapshotTesting"),
+                .target(name: "VisualTestKit")
+            ],
             path: "Tests",
             exclude: ["Snapshots"]
             ),
